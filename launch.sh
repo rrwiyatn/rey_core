90b3cde7e601f73f7e89fde02c8956032e39a67f
rrwiyatn <rey.reza@hotmail.com>
2019-10-07 15:46:14 -0400

    edit typo in launch.sh

--- launch.sh
+++ launch.sh
@@ -5,4 +5,4 @@ set -e
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
 echo "This is an empty launch script. Update it to launch your application."
-roslaunch rey_pure_pursuit pure_pursuit.launch
+roslaunch rey_pure_pursuit rey_pure_pursuit.launch
