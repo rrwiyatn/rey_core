04a516bf8c2f1b9946ff29c28cd0ece54fc23def
rrwiyatn <rey.reza@hotmail.com>
2019-10-14 23:59:10 -0400

    fix bugs

--- packages/rey_pure_pursuit/nodes/pure_pursuit.py
+++ packages/rey_pure_pursuit/nodes/pure_pursuit.py
@@ -16,11 +16,11 @@ class PurePursuit():
         
         # Add subscriber(s) # TODO: change topic name, message type, callback name
         # self.line_sub = rospy.Subscriber('/default/ground_projection/lineseglist_out', SegmentList, self.pure_pursuit_callback, queue_size = 1)
-        self.line_sub = rospy.Subscriber('/default/lane_filter_node/seglist_filtered', SegmentList, self.pure_pursuit_callback, queue_size = 1)
+        self.line_sub = rospy.Subscriber('/bebek/lane_filter_node/seglist_filtered', SegmentList, self.pure_pursuit_callback, queue_size = 1)
         # self.lane_pose_sub = rospy.Subscriber('/default/lane_filter_node/lane_pose', LanePose, self.lane_pose_callback, queue_size = 1)
 
         # Add publisher(s)  # TODO: change topic name
-        self.car_cmd_pub = rospy.Publisher('/default/joy_mapper_node/car_cmd', Twist2DStamped, queue_size=1)
+        self.car_cmd_pub = rospy.Publisher('/bebek/joy_mapper_node/car_cmd', Twist2DStamped, queue_size=1)
 
 
         # To store subscribed data
