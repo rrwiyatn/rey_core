16361a262a4a2d1037eb6ce5ba3e535af9873bfe
rrwiyatn <rey.reza@hotmail.com>
2019-10-16 14:35:38 -0400

    param adjust again

--- packages/rey_pure_pursuit/nodes/pure_pursuit.py
+++ packages/rey_pure_pursuit/nodes/pure_pursuit.py
@@ -12,7 +12,7 @@ class PurePursuit():
         self.K_white = 0.3 # 0.15 - 0.1
         self.K_yellow = 0.4 # 0.3 - 0.3
         self.num_lines_th = 2 # 2
-        self.offset_white = 0.5 # 0.7 - 1.1 
+        self.offset_white = 1.1 # 0.7 - 1.1 
         self.offset_yellow = 0.3 # 0.3 - 0.15
         self.v = 0.4 # 0.3 - 0.5
         
