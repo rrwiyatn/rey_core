409202caa09b6d512407aa87bb46234d0d16a7f9
rrwiyatn <rey.reza@hotmail.com>
2019-10-15 22:06:38 -0400

    fix bugs

--- packages/rey_pure_pursuit/nodes/pure_pursuit.py
+++ packages/rey_pure_pursuit/nodes/pure_pursuit.py
@@ -14,7 +14,7 @@ class PurePursuit():
         self.num_lines_th = 2
         self.offset_white = 0.7
         self.offset_yellow = 0.3
-        self.v = 0.3
+        self.v = 0.5
         
         # Add subscriber(s) # TODO: change topic name, message type, callback name
         # self.line_sub = rospy.Subscriber('/default/ground_projection/lineseglist_out', SegmentList, self.pure_pursuit_callback, queue_size = 1)
