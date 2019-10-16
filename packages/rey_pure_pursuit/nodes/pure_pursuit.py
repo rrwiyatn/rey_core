ad7d659bbe3e8699a1b41bfa8389877051dff023
rrwiyatn <rey.reza@hotmail.com>
2019-10-16 16:09:58 -0400

    param adjust again

--- packages/rey_pure_pursuit/nodes/pure_pursuit.py
+++ packages/rey_pure_pursuit/nodes/pure_pursuit.py
@@ -10,9 +10,9 @@ class PurePursuit():
         rospy.init_node('pure_pursuit_node', anonymous=True)
 
         self.K_white = 0.225 # 0.15 - 0.1
-        self.K_yellow = 0.17 # 0.3 - 0.3
+        self.K_yellow = 0.16 # 0.3 - 0.3
         self.num_lines_th = 2 # 2
-        self.offset_white = 0.65 # 0.7 - 1.1 
+        self.offset_white = 0.6 # 0.7 - 1.1 
         self.offset_yellow = 0.6 # 0.3 - 0.15
         self.v = 0.25 # 0.3 - 0.5
         
